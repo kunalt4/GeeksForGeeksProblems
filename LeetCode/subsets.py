cd8e97fe3e29b5ccbee82201ba6887301b57608a
kunalt4 <kunaltolani17@gmail.com>
2020-03-19 22:40:31 +0000

    subsets

---
+++ LeetCode/subsets.py
@@ -0,0 +1,7 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = [[]]
+
+        for num in nums:
+            result += [i + [num] for i in result]
+        return result
