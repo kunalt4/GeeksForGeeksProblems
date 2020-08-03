56e80e647dcd9b14a8cbcb40e066cb4a96b53930
Kunal Tolani <22201536+kunalt4@users.noreply.github.com>
2020-08-03 13:34:51 +0100

    Create intersection_of_two_arrays.py

---
+++ LeetCode/intersection_of_two_arrays.py
@@ -0,0 +1,13 @@
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        result = []
+        count = {}
+        
+        for num in nums1:
+            count[num]=1
+        
+        for num in nums2:
+            if num in count and count[num]>0:
+                result.append(num)
+                count[num]-=1
+        return result
