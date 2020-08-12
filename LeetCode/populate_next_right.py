3f2f28ae86300e755097d0fb959ed459d703abc2
Kunal Tolani <22201536+kunalt4@users.noreply.github.com>
2020-08-12 23:41:33 +0100

    Create populate_next_right.py

---
+++ LeetCode/populate_next_right.py
@@ -0,0 +1,33 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        
+        if not root:
+            return None
+        
+        que = []
+        que.append(root)
+        
+        while(len(que) > 0):
+            
+            temp = que
+            for t in range(len(temp)-1):
+                temp[t].next = temp[t+1]
+            que = []
+            for t in temp:
+                if t.left is not None:
+                    que.append(t.left)
+
+                if t.right is not None:
+                    que.append(t.right)
+        return root
+            
