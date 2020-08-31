b113c9beefbfd7fe65748fd1f1997fed1aae1fe0
Kunal Tolani <22201536+kunalt4@users.noreply.github.com>
2020-08-31 01:49:53 +0100

    Create num_decodings.py

---
+++ LeetCode/num_decodings.py
@@ -0,0 +1,23 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        if not s:
+            return 0
+        
+        dp = [0 for _ in range(len(s)+1)]
+        
+        dp[0] = 1
+        
+        if s[0] == "0":
+            dp[1] = 0
+        else:
+            dp[1] = 1
+            
+        for i in range(2, len(s)+1):
+            
+            if int(s[i-1:i]) > 0 and int(s[i-1:i]) <=9:
+                dp[i]+=dp[i-1]
+                
+            if int(s[i-2:i]) >= 10 and int(s[i-2:i])<=26:
+                dp[i]+=dp[i-2]
+                
+        return dp[-1]
